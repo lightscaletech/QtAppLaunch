f93e088ae41951cb98412e7c03e9da940d4b4df9
Sam Light <samlight1994@gmail.com>
2015-11-21 13:04:10 +0000

    WidgResults.cpp: Displayed results.

--- src/WidgResults.cpp
+++ src/WidgResults.cpp
@@ -1,6 +1,9 @@
 #include "WidgResults.h"
 
+#include "ApplicationShortcut.h"
+
 #include <QVBoxLayout>
+#include <QLabel>
 
 WidgResults::WidgResults(QWidget * parent):
 QWidget(parent),
@@ -14,12 +17,23 @@ WidgResults::~WidgResults(){}
 
 void WidgResults::show(const AppList & list)
 {
+	clear();
 	if(list.empty()) return;
 
+	QLabel * lab = NULL;
 
+	for (auto item : list)
+	{
+		lab = new QLabel(this);
+		lab->setText(item->getApplicationName());
+
+		layout->addWidget(lab);
+	}
+	QWidget::show();
 }
 
 void WidgResults::clear()
 {
-
+	foreach (QWidget * w, this->findChildren<QWidget*>()) delete w;
+	this->hide();
 }
