efe288bd5b3dc16cbd8a81feccaad18c3bddaa76
Sam Light <samlight1994@gmail.com>
2015-12-09 20:42:49 +0000

    WidgResults.*: Fixed next previous.

--- src/WidgResults.cpp
+++ src/WidgResults.cpp
@@ -1,13 +1,15 @@
 #include "WidgResults.h"
 
 #include "ApplicationShortcut.h"
+#include "WidgResultItem.h"
 
 #include <QVBoxLayout>
-#include <QLabel>
+#include <QPalette>
 
 WidgResults::WidgResults(QWidget * parent):
 QWidget(parent),
-layout(NULL)
+layout(NULL),
+selected(NULL)
 {
 	layout = new QVBoxLayout();
 	this->setLayout(layout);
@@ -15,25 +17,91 @@ layout(NULL)
 
 WidgResults::~WidgResults(){}
 
+void WidgResults::clear()
+{
+	foreach (QWidget * w, this->findChildren<QWidget*>()) delete w;
+	this->hide();
+	selected = NULL;
+	this->update();
+}
+
 void WidgResults::show(const AppList & list)
 {
 	clear();
 	if(list.empty()) return;
 
-	QLabel * lab = NULL;
+	WidgResultItem * resItem = NULL;
 
 	for (auto item : list)
 	{
-		lab = new QLabel(this);
-		lab->setText(item->getApplicationName());
+		resItem = new WidgResultItem(this);
+		resItem->setText(item->getApplicationName());
+		resItem->setApp(item);
+		resItem->setAutoFillBackground(true);
 
-		layout->addWidget(lab);
-	}
+		layout->addWidget(resItem);
+	}	
+	next();
 	QWidget::show();
 }
 
-void WidgResults::clear()
+void WidgResults::select(int index)
 {
-	foreach (QWidget * w, this->findChildren<QWidget*>()) delete w;
-	this->hide();
+	QLayoutItem * lItem = layout->itemAt(index);
+	QWidget * widg = lItem->widget();
+	if(!widg) return;
+
+	WidgResultItem * item =  static_cast<WidgResultItem *>(widg);
+
+	if(item == selected) return;
+
+	QPalette pal = item->palette();
+	pal.setColor(item->backgroundRole(), QColor(0xC0, 0xC0, 0xC0));
+
+	item->setPalette(pal);
+
+	unselect();
+	selected = item;
+}
+
+void WidgResults::unselect()
+{
+	if(!selected) return;
+
+	QPalette pal = selected->palette();
+	pal.setColor(selected->backgroundRole(), this->palette().color(this->backgroundRole()));
+
+
+	selected->setPalette(pal);
+
+	selected = NULL;
+}
+
+void WidgResults::next()
+{
+	int index = 0;
+
+	if(selected) index = layout->indexOf(selected) + 1;
+
+	if(index >= layout->count()) index = 0;
+	else if(index < 0) index = 0;
+
+	select(index);
+}
+
+void WidgResults::prev()
+{
+	int index = layout->count();
+
+	if(selected) index = layout->indexOf(selected) - 1;
+
+	if(index >= layout->count()) index = layout->count() - 1;
+	else if(index < 0) index = layout->count() - 1;
+
+	select(index);
+}
+
+void WidgResults::run()
+{
+	selected->getApp()->run();
 }
--- src/WidgResults.h
+++ src/WidgResults.h
@@ -6,18 +6,31 @@
 #include <QWidget>
 
 class QVBoxLayout;
+class WidgResultItem;
 
 class WidgResults:
 public QWidget
 {
+	Q_OBJECT
 	public:
 		WidgResults(QWidget * = NULL);
 		~WidgResults();
 
 		void show(const AppList &);
 		void clear();
+		
+		void select(int);
+		void unselect();
+
+	public slots:
+
+		void next();
+		void prev();
+		void run();
+
 	private:
 		QVBoxLayout * layout;
+		WidgResultItem * selected;
 };
 
 #endif
